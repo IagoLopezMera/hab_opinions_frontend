a43dba8c38cff8f9aa6b7213cf7de88ade811410
Alexander <50184502+alexander970@users.noreply.github.com>
2022-06-01 22:00:55 +0200

    Inital register form structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import {LoginPage} from './pages/LoginPage';
+import { RegisterPage } from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
     <main>Portal de Opiniones</main>
     <LoginPage/>
+    <RegisterPage />
     </>
   );
 }
