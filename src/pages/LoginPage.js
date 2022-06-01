e6dcd0da7ddc1183bb4bb654a22f80bf585db26e
Alexander <50184502+alexander970@users.noreply.github.com>
2022-06-01 20:19:58 +0200

    Initial Login structure

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+export const LoginPage = () => {
+    const [email,setEmail]= useState("");
+    const handleEmailChange = (e) => setEmail(e.target.value);
+    const [password,setPassword]= useState("");
+    const handlePasswordChange = (e) => setPassword(e.target.value);
+
+    const handleFormSubmit= async(e)=>{
+        e.preventDefault();
+        //TODO: make API call (Login)
+
+    }
+
+    return (
+        <section>
+            <h1>Sign in</h1>
+            <form onSubmit= {handleFormSubmit}>
+                <fieldset>
+                    <label htmlFor = "email" >Email</label>
+                    <input type="email" id ="email" name="email" required onChange={handleEmailChange}/>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor = "password" >Password</label>
+                    <input type="password" id ="password" name="password" required onChange={handlePasswordChange}/>
+                </fieldset>
+
+                <button>Sign in</button>
+            </form>
+        </section>
+        
+    );
+};
